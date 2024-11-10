b80e81fdc95a9902c873d6cf4b007af8cc3c2540
richieb21 <76607899+richieb21@users.noreply.github.com>
2024-11-10 00:09:44 -0500

    EVERYTHING WORKS

--- backend/match.py
+++ backend/match.py
@@ -7,21 +7,21 @@ import os
 from flask import Flask, Blueprint
 
 def get_user_with_profile(user_id):
-    user_data = users_collection.find_one({"user_id": ObjectId(user_id)})
+    print(user_id)
+    user_data = users_collection.find_one({"_id": ObjectId(user_id)})
     profile_data = user_profiles_collection.find_one({"user_id": ObjectId(user_id)})
 
     if user_data and profile_data:
-        return User.from_mongo(user_data, profile_data)
+        return profile_data
     else:
         print("User or profile not found.")
         return None
 
 def jobMatch(user, job):
     user_object = get_user_with_profile(user)
-    print(user_object)
-    print(user_object.skills)
 
     job_key_words = extractPhrases(job)
+    
     
     
 
